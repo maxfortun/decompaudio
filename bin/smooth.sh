5a6a99c881d9df2bee2e1a385ce9b02084cf7cdd
Max <max@maxf.net>
2018-03-18 20:26:30 -0400

    Added

---
+++ bin/smooth.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+size=$1
+file=${2:--}
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <size> <file>"
+	exit
+fi
+
+declare -a samples
+sampledSize=0
+index=0
+sum=0
+sumDiffSq=0
+mean=0
+variance=0
+deviation=0
+
+while read sample; do
+	if [ "$sampledSize" == "$size" ]; then
+		oldSample=${samples[$index]};
+		#echo "old $index $oldSample"
+		oldSampleDelta=$(( $oldSample - $mean ))
+		oldSampleDiffSq=$(( oldSampleDelta * oldSampleDelta ))
+		sum=$(( sum - oldSample ))
+		sumDiffSq=$(( sumDiffSq - oldSampleDiffSq ))
+		sumDiffSq=${sumDiffSq#-}
+	fi
+
+	if [ "$sampledSize" -lt "$size" ]; then
+		sampledSize=$(( sampledSize + 1 ))
+	fi
+
+	sum=$(( sum + sample ))
+	mean=$(( sum / sampledSize ))
+
+	#echo $sample $sumDiffSq
+	sampleDelta=$(( sample - mean ))
+	sampleDiffSq=$(( sampleDelta * sampleDelta ))
+	sumDiffSq=$(( sumDiffSq + sampleDiffSq ))
+
+	variance=$(( sumDiffSq / sampledSize ))
+	deviation=$( echo "sqrt($variance)"|bc)
+
+	if [ -z "$deviation" ]; then
+		echo "bad variance $sumDiffSq $sampledSize sqrt($variance)" >&2
+		exit
+	fi
+
+	samples[$index]=$sample
+	index=$(( index + 1 ))
+
+	if [ "$index" == "$size" ]; then
+      index=0
+	fi
+
+	#printf "%d $sample $sumDiffSq $index/$sampledSize\n" $mean
+	printf "%x\n" $mean
+
+done < <(cat $file)
+
+
+exit
+
+
+
